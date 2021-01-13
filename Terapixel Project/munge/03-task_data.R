838d4585e4dc0e858b1bbe5410acb2d554a296ee
LukeBattle <mail@lukebattle.com>
2021-01-13 13:31:44 +0000

    Removed pixel summary from task_data munge file

--- Terapixel Project/munge/03-task_data.R
+++ Terapixel Project/munge/03-task_data.R
@@ -8,15 +8,4 @@ all_data = left_join(gpu_app_data,task_data[c("taskId","x","y","level")],by = c(
 
 cache('all_data')
 
-#create summary of average properties per pixel from task data
 
-pixel_summary = gpu_task_app_data %>%
-  group_by(x,y,level) %>%
-  summarise(runtime = mean(runtime),
-            powerdrain = mean(powerDrawWatt),
-            temp = mean(gpuTempC),
-            memutil = mean(gpuMemUtilPerc),
-            gpuutil = mean(gpuUtilPerc)) %>%
-  arrange(desc(runtime))
-
-cache('pixel_summary')
